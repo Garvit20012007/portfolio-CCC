8bf68b230c2540863cb1b6fc254480ee2dbad44b
Garvit Sharma <148376518+Garvit20012007@users.noreply.github.com>
2024-10-25 23:25:16 +0530

    Add files via upload

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/Garvit.jpg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi, I'm Garvit Sharma, basically belongs to a city which is famous for Locks in India. Currently, pursuing B. Tech. in CSIT at Ajak Kumar Garg Engineering College, Ghaziabad. 
+          </p>
+          <p className="hero--section-description">
+            My Skills include Html, CSS, JavaScript, MERN Stack - Learning, my hobbies are Badminton, Chess and Cooking.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm John</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Hello Everyone, I'm Garvit Sharma. I'm a Font End Developer, Designer & Freelancer.
+            <br /> Get in Touch for more conversation.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/Garvit.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
